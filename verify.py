f200209e759759669c302ca9433ad3a17be3b660
Johan <j.meppelink@datarefinery.amsterdam>
2025-04-15 07:17:47 +0200

    add verify

--- verify.py
+++ verify.py
@@ -5,12 +5,12 @@ from loguru import logger
 from verify_modes import ASSERT_MODE, VerifyMode
 
 
-def verify(check: Callable, message: str = ""):
+def verify(cond: Callable, message: str = ""):
     """The condition_func should return a boolean value. Note it needs to be a callable not a boolean value itself.
     In front of your condition you could do lambda: <condition> to make it a callable."""
     if ASSERT_MODE == VerifyMode.SKIP:
         return
-    result = check()
+    result = cond()
     if not result:
         if ASSERT_MODE == VerifyMode.LOG:
             logger.critical(f"Assertion failed: {message}")
